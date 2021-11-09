da1364e11700aa04f69c7e00a3690d428959f79e
Mykyta Kucenko <m.kucenko@bvblogic.com>
2021-11-09 15:02:06 +0200

    Сборка проекта при помощи Gulp
    1)Выполнить команду для глобальной установки Gulp
    npm install -g gulp-cli
    2)Иницелизация файла package.json
    npm init
    3)Установка модулей
    npm install gulp gulp-autoprefixer browser-sync del gulp-cheerio gulp-imagemin gulp-plumber gulp-replace sass gulp-sass gulp-sourcemaps gulp-svg-sprite gulp-svgmin imagemin-jpeg-recompress imagemin-pngquant run-sequence --save-dev

---
+++ gulpfile.babel.js
@@ -0,0 +1,157 @@
+import gulp from 'gulp';
+const sass = require('gulp-sass')(require('sass'));
+import del from 'del';
+import plumber from 'gulp-plumber';
+import sourceMaps from 'gulp-sourcemaps';
+import autoprefixer from 'gulp-autoprefixer';
+import browserSync from 'browser-sync';
+// import imagemin from 'gulp-imagemin';
+// import imageminJpegRecompress from 'imagemin-jpeg-recompress';
+// import pngquant from 'imagemin-pngquant';
+import svgmin from 'gulp-svgmin';
+import cheerio from "gulp-cheerio";
+import replace from "gulp-replace";
+import svgSprite from "gulp-svg-sprite";
+
+// const gulp = require('gulp');
+// const sass = require('gulp-sass');
+// const plumber = require('gulp-plumber');
+// const autoprefixer = require('gulp-autoprefixer');
+// const browserSync = require('browser-sync').create();
+// const sourceMaps = require('gulp-sourcemaps');
+// const imagemin = require("gulp-imagemin");
+// const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+// const pngquant = require('imagemin-pngquant');
+// const run = require("run-sequence");
+// const del = require("del");
+// const svgSprite = require('gulp-svg-sprite');
+// const svgmin = require('gulp-svgmin');
+// const cheerio = require('gulp-cheerio');
+// const replace = require('gulp-replace');
+
+const paths = {
+    styles: {
+        src: 'scss/style.scss',
+        dest: 'build/css/'
+    },
+    scripts: {
+        src: 'js/!**!/!*.js',
+        dest: 'build/scripts/'
+    },
+    css: {
+        src: 'css/!**!/!*.css',
+        dest: 'build/css/'
+    }
+};
+
+/*
+ * For small tasks you can export arrow functions
+ */
+export const clean = () => del(['build']);
+
+/*
+ * You can also declare named functions and export them as tasks
+ */
+export function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(plumber())
+        .pipe(sourceMaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+export function html() {
+    return gulp.src('*.html')
+        .pipe(gulp.dest('build'))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+export function script() {
+    return gulp.src(paths.scripts.src)
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+export function stylesCss() {
+    return gulp.src(paths.css.src)
+        .pipe(gulp.dest(paths.css.dest))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+export function allimg() {
+    return gulp.src('img/*.{png,jpg}')
+        .pipe(gulp.dest('build/img'))
+        .pipe(browserSync.reload({stream: true}));
+}
+
+/*export function images() {
+    return gulp.src('build/img/!**!/!*.{png,jpg}')
+        // .pipe(imagemin([
+        //     imagemin.jpeg({progressive: true}),
+        //     imageminJpegRecompress({
+        //         loops: 5,
+        //         min: 65,
+        //         max: 70,
+        //         quality: 'medium'
+        //     }),
+        //     imagemin.optipng({optimizationLevel: 3}),
+        //     // pngquant({quality: '65-70', speed: 5})
+        // ]))
+        .pipe(gulp.dest('build/img'));
+}*/
+
+export function svg(){
+    return gulp.src('img/**/*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: "sprite.svg"
+                }
+            }
+        }))
+        .pipe(gulp.dest('build/img'));
+}
+
+export function serve(){
+    browserSync.init({server: "build"});
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch("*.html", html);
+    gulp.watch(paths.scripts.src, script);
+    gulp.watch("css/**/*.css", stylesCss);
+    gulp.watch("img/**/*.{png,jpg}", allimg);
+    gulp.watch("img/**/*.{svg}", svg);
+}
+
+const build = gulp.series(clean, gulp.parallel(
+    styles,
+    html,
+    script,
+    stylesCss,
+    allimg,
+    // images
+    svg
+    ));
+exports.build = build;
+
+/*
+ * Export a default task
+ */
+export default build;
